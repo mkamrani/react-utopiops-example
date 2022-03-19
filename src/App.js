5798118880fb8d4a75855f362d066bc6727a66b5
Mohsen Kamrani <m.kamrani@hotmail.com>
2022-03-19 20:46:05 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+    Use cloud without managing cloud.
         </p>
         <a
           className="App-link"
